0539d45bf52a48d5957616914fc8e0436885a396
diego <diegombar@github.com>
2017-06-01 14:06:54 +0100

    added DQN for gridworld

--- scripts/deepRL/gridworld.py
+++ scripts/deepRL/gridworld.py
@@ -1,5 +1,4 @@
 import numpy as np
-import random
 import itertools
 import scipy.misc
 import matplotlib.pyplot as plt
@@ -22,38 +21,38 @@ class gameEnv():
         self.actions = 4
         self.objects = []
         self.partial = partial
-        a = self.reset()
+        a, state = self.reset()
         plt.imshow(a,interpolation="nearest")
         
         
     def reset(self):
         self.objects = []
-        hero = gameOb(self.newPosition(),1,1,2,None,'hero')
+        hero = gameOb((0,0),1,1,2,None,'hero')
         self.objects.append(hero)
-        bug = gameOb(self.newPosition(),1,1,1,1,'goal')
-        self.objects.append(bug)
-        hole = gameOb(self.newPosition(),1,1,0,-1,'fire')
+        goal = gameOb((3,3),1,1,1,1,'goal')
+        self.objects.append(goal)
+        hole = gameOb((2,1),1,1,0,-1,'fire')
         self.objects.append(hole)
-        hole2 = gameOb(self.newPosition(),1,1,0,-1,'fire')
+        hole2 = gameOb((1,2),1,1,0,-1,'fire')
         self.objects.append(hole2)
-        hole3 = gameOb(self.newPosition(),1,1,0,-1,'fire')
+        hole3 = gameOb((3,0),1,1,0,-1,'fire')
         self.objects.append(hole3)
         # bug2 = gameOb(self.newPosition(),1,1,1,1,'goal')
         # self.objects.append(bug2)
         # bug3 = gameOb(self.newPosition(),1,1,1,1,'goal')
         # self.objects.append(bug3)
         # bug4 = gameOb(self.newPosition(),1,1,1,1,'goal')
-        # self.objects.append(bug4)
-        state = self.renderEnv()
+        # self.objects`.append(bug4)
+        a, state = self.renderEnv()
         self.state = state
-        return state
+        return a, state
 
     def moveChar(self,direction):
         # 0 - up, 1 - down, 2 - left, 3 - right
         hero = self.objects[0]
         heroX = hero.x
         heroY = hero.y
-        penalize = 0.
+        penalize = -0.01
         if direction == 0 and hero.y >= 1:
             hero.y -= 1
         if direction == 1 and hero.y <= self.sizeY-2:
@@ -61,12 +60,13 @@ class gameEnv():
         if direction == 2 and hero.x >= 1:
             hero.x -= 1
         if direction == 3 and hero.x <= self.sizeX-2:
-            hero.x += 1     
+            hero.x += 1
         if hero.x == heroX and hero.y == heroY:
-            penalize = 0.0
+            penalize = -0.01
         self.objects[0] = hero
         return penalize
     
+    # randomly pick a new position
     def newPosition(self):
         iterables = [ range(self.sizeX), range(self.sizeY)]
         points = []
@@ -74,11 +74,11 @@ class gameEnv():
             points.append(t)
         currentPositions = []
         for objectA in self.objects:
-            if (objectA.x,objectA.y) not in currentPositions:
-                currentPositions.append((objectA.x,objectA.y))
+            if (objectA.x, objectA.y) not in currentPositions:
+                currentPositions.append((objectA.x, objectA.y))
         for pos in currentPositions:
             points.remove(pos)
-        location = np.random.choice(range(len(points)),replace=False)
+        location = np.random.choice(range(len(points)), replace=False)
         return points[location]
 
     def checkGoal(self):
@@ -91,14 +91,9 @@ class gameEnv():
         ended = False
         for other in others:
             if hero.x == other.x and hero.y == other.y:
-                self.objects.remove(other)
-                if other.reward == 1:
-                    self.objects.append(gameOb(self.newPosition(),1,1,1,1,'goal'))
-                else: 
-                    self.objects.append(gameOb(self.newPosition(),1,1,0,-1,'fire'))
-                return other.reward,False
-        if ended == False:
-            return 0.0,False
+                return other.reward, True
+        if not ended:
+            return 0.0, False
 
     def renderEnv(self):
         #a = np.zeros([self.sizeY,self.sizeX,3])
@@ -115,16 +110,11 @@ class gameEnv():
         c = scipy.misc.imresize(a[:,:,1],[84,84,1],interp='nearest')
         d = scipy.misc.imresize(a[:,:,2],[84,84,1],interp='nearest')
         a = np.stack([b,c,d],axis=2)
-        return a
+        heroPos = hero.x + self.sizeX * hero.y
+        return a, heroPos
 
-    def step(self,action):
+    def step(self, action):
         penalty = self.moveChar(action)
-        reward,done = self.checkGoal()
-        state = self.renderEnv()
-        if reward == None:
-            print(done)
-            print(reward)
-            print(penalty)
-            return state,(reward+penalty),done
-        else:
-            return state,(reward+penalty),done
+        reward, done = self.checkGoal()
+        a, heroPos = self.renderEnv()
+        return heroPos, (reward + penalty), done, a
