2e02303e9a77f4d5f28d058b89cac825e2b51443
diego <diegombar@github.com>
2017-06-13 00:39:15 +0100

    test setTargetVel with key pressing working

--- scripts/v-rep_project/micoArmClientScript.py
+++ scripts/v-rep_project/micoArmClientScript.py
@@ -19,9 +19,24 @@
 # targetPos3={math.pi,math.pi,math.pi,math.pi,math.pi,math.pi}
 # simRMLMoveToJointPositions(jointHandles,-1,currentVel,currentAccel,maxVel,maxAccel,maxJerk,targetPos3,targetVel,{-1,0,0,1,1,1})
 
+
+
+
+# connect to V-Rep Remote Api Server
+# clientID=vrep.simxStart(..........)
+
+# Start simulation
+# vrep.simxStartSimulation(clientID,vrep.simx_opmode_blocking)
+
+
+
+
+
+
 import vrep
 import sys
 import time
+import readchar
 
 print('Mico Arm Program started')
 
@@ -35,20 +50,20 @@ vel = 35
 accel = 10
 jerk = 5
 
-currentVel = [0]*6
-currentAccel = [0]*6
-maxVel = [vel*pi/180]*6
-maxAccel = [accel*pi/180]*6
-maxJerk = [jerk*pi/180]*6
-targetVel = [0]*6
+currentVel = [0] * 6
+currentAccel = [0] * 6
+maxVel = [vel*pi/180] * 6
+maxAccel = [accel*pi/180] * 6
+maxJerk = [jerk*pi/180] * 6
+targetVel = [0] * 6
 
-targetPos1=[90*pi/180]*6
-targetPos2=[90*pi/180,135*pi/180,225*pi/180,180*pi/180,180*pi/180,350*pi/180]
-targetPos3=[pi]*6
-targetPos4=[pi,135*pi/180,225*pi/180,180*pi/180,180*pi/180,350*pi/180]
+targetPos1 = [90*pi/180] * 6
+targetPos2 = [90*pi/180, 135*pi/180, 225*pi/180, 180*pi/180, 180*pi/180,350*pi/180]
+targetPos3 = [pi] * 6
+targetPos4 = [pi, 135*pi/180, 225*pi/180, 180*pi/180, 180*pi/180, 350*pi/180]
 
 #Hand
-closingVel=-0.04
+closingVel = -0.04
 # if (not closing) then
 #     simSetJointTargetVelocity(j0,-closingVel)
 #     simSetJointTargetVelocity(j1,-closingVel)
@@ -56,6 +71,9 @@ closingVel=-0.04
 #     simSetJointTargetVelocity(j0,closingVel)
 #     simSetJointTargetVelocity(j1,closingVel)
 # end
+
+#testing setTargetVelocity
+tVel = 2.0
 
 if len(sys.argv) >= 10:
     portNb = int(sys.argv[1])
@@ -82,23 +100,42 @@ def setJointTargetPositions(clientID, targetPositions):
             print("SetJointTargetPosition got error code: %s" % errorCode)
 
 
-
 vrep.simxFinish(-1)# close all opened connections
 clientID = vrep.simxStart('127.0.0.1', portNb, True, True, 2000, 5)
 if clientID != -1:
     print('Connected to remote API server')
     while vrep.simxGetConnectionId(clientID) != -1:
-        closeHand(clientID)
-        setJointTargetPositions(clientID, targetPos1)
-        time.sleep(2.0) #in sec
+        # closeHand(clientID)
+        # setJointTargetPositions(clientID, targetPos1)
+        # errorCode = vrep.simxSetJointTargetVelocity(clientID, jointHandles[0], tVel, vrep.simx_opmode_blocking)
+        # if errorCode != vrep.simx_return_ok:
+        #     print("SetJointTargetVelocity got error code: %s" % errorCode)
+        # time.sleep(2.0) #in sec
 
-        openHand(clientID)
-        setJointTargetPositions(clientID, targetPos2)
-        time.sleep(2.0) #in sec
+        # openHand(clientID)
+        # setJointTargetPositions(clientID, targetPos2)
+        # time.sleep(2.0) #in sec
 
-        closeHand(clientID)
-        setJointTargetPositions(clientID, targetPos3)
-        time.sleep(2.0) #in sec
+        # closeHand(clientID)
+        # setJointTargetPositions(clientID, targetPos3)
+        # time.sleep(2.0) #in sec
+
+        # testing settargetvel with arrow pressing
+        c = readchar.readchar()
+        if c == 'a':    
+            return_code = vrep.simxSetJointTargetVelocity(clientID, jointHandles[5], tVel, vrep.simx_opmode_blocking)
+            if errorCode != vrep.simx_return_ok:
+                print("SetJointTargetPosition got error code: %s" % errorCode)
+        elif c == 'd':
+            errorCode = vrep.simxSetJointTargetVelocity(clientID, jointHandles[5], -tVel, vrep.simx_opmode_blocking)
+            if errorCode != vrep.simx_return_ok:
+                print("SetJointTargetPosition got error code: %s" % errorCode)
+        elif c == 's':
+            errorCode = vrep.simxSetJointTargetVelocity(clientID, jointHandles[5], 0, vrep.simx_opmode_blocking)
+            if errorCode != vrep.simx_return_ok:
+                print("SetJointTargetPosition got error code: %s" % errorCode)
+        print('char=',c)       # --> gives me "0"
+        time.sleep(0.1)
 
     vrep.simxFinish(clientID)
 else:
